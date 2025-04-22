ba2daf0b939fd302b4e7bacdb26f4d3b6f80b2b6
mohammed-switi <switi.2003@gmail.com>
2025-04-22 17:33:34 +0300

    Created Restaurant Model

--- RestaurantReservation.Db/Models/Restaurant.cs
+++ RestaurantReservation.Db/Models/Restaurant.cs
@@ -1,6 +1,14 @@
 namespace RestaurantReservation.Db.Models;
 
-public class Restaurant
-{
-    
+public class Restaurant {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public string OpeningHours { get; set; }
+
+    public ICollection<Table> Tables { get; set; }
+    public ICollection<Employee> Employees { get; set; }
+    public ICollection<MenuItem> MenuItems { get; set; }
+    public ICollection<Reservation> Reservations { get; set; }
 }

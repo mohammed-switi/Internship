e9555e5c4599a72ca5177c8843542dd018549dc2
mohammed-switi <switi.2003@gmail.com>
2025-04-22 17:30:59 +0300

    Created Menu Order Model

--- RestaurantReservation.Db/Models/Order.cs
+++ RestaurantReservation.Db/Models/Order.cs
@@ -1,6 +1,14 @@
 namespace RestaurantReservation.Db.Models;
 
-public class Order
-{
-    
+
+public class Order {
+    public int OrderId { get; set; }
+    public int ReservationId { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public Reservation Reservation { get; set; }
+    public Employee Employee { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
 }

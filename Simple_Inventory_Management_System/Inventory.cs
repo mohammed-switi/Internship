b58e150e09d24277912ff127ea71267ba9f5ac1b
mohammed-switi <switi.2003@gmail.com>
2025-04-22 16:44:44 +0300

    Implemented new functionality to add product to the Mongo Database

--- Simple_Inventory_Management_System/Inventory.cs
+++ Simple_Inventory_Management_System/Inventory.cs
@@ -14,5 +14,15 @@ class Inventory
         _productCollection = database.GetCollection<Product>("Products");
     }
     
+    public void AddProduct(string name, double price, int quantity)
+    {
+        var product = new Product(name, price, quantity);
+        _productCollection.InsertOne(product);
+        Console.WriteLine("Product added successfully.");
+    }
+    
+    
+    
+    
     
 }

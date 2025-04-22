de3a82c6148974103e0963e0ccded51ccc2a369c
mohammed-switi <switi.2003@gmail.com>
2025-04-22 17:30:23 +0300

    Created Menu Item Model

--- RestaurantReservation.Db/Models/MenuItem.cs
+++ RestaurantReservation.Db/Models/MenuItem.cs
@@ -1,7 +1,11 @@
 namespace RestaurantReservation.Db.Models;
+public class MenuItem {
+    public int ItemId { get; set; }
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
 
-public class MenuItem
-{
-    
-    
+    public Restaurant Restaurant { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
 }

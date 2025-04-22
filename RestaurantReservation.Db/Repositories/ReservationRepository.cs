f2f2bcde5357b873f9044939755b77d1e41633e9
mohammed-switi <switi.2003@gmail.com>
2025-04-22 23:27:42 +0300

    Created Reservation Respository

--- RestaurantReservation.Db/Repositories/ReservationRepository.cs
+++ RestaurantReservation.Db/Repositories/ReservationRepository.cs
@@ -1,6 +1,24 @@
-namespace RestaurantReservation.Db.Repositories;
 
-public class ReservationRepository
-{
-    
+using RestaurantReservation.Db.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.Repositories {
+    public class ReservationRepository : BaseRepository<Reservation> {
+        public ReservationRepository(RestaurantReservationDbContext ctx) : base(ctx) { }
+
+        public async Task<List<Reservation>> GetByCustomerAsync(int customerId) {
+            return await _ctx.Reservations
+                .Include(r => r.Restaurant)
+                .Include(r => r.Customer)
+                .Where(r => r.CustomerId == customerId)
+                .ToListAsync();
+        }
+
+        public async Task<List<ReservationCustomerRestaurantView>> ListReservationCustomerRestaurantAsync() {
+            return await _ctx.ReservationCustomerRestaurant.ToListAsync();
+        }
+    }
 }
